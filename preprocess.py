acfd63e0171ba71b809a5546fa20ae4f6ff5d0a0
czyi <czy19960126@gmail.com>
2017-11-21 13:49:12 -0800

    m

--- preprocess.py
+++ preprocess.py
@@ -10,10 +10,32 @@ import types
 
 
 def copy_and_rename(fpath_input, fpath_output):
+    folder_list = []
     for folder in os.listdir(fpath_input):
         folder_name = os.path.splitext(folder)[0]
-        print(type(folder_name))
-        print(folder_name)
+        # print(type(folder_name))
+        # print(folder_name)
+        folder_list.append(fpath_input+'/'+folder_name+"/iPhone6-1L")
+        folder_list.append(fpath_input+'/'+folder_name+"/iPhone6-2R")
+
+    print(len(folder_list))
+
+    for folder_path in folder_list:
+        for folder in os.listdir(folder_path):
+            if(folder[0]!='T'):
+                continue
+            print(folder)
+            for file in os.listdir(folder_path+'/'+folder):
+                if(file=='iPhoneSensors.csv'):
+                    # print('\t', file)
+                    oldname = os.path.join(folder_path+'/'+folder, file)
+                    newname = os.path.join(fpath_output+'/sensor', os.path.splitext(file)[0]+'_'+folder+".csv")
+                    print(oldname)
+                    print(newname)
+                    shutil.copy(oldname, newname)
+        print('===========')
+
+
 
         # #if os.path.splitext(file)[1] == ".jpg":
         # oldname = os.path.join(fpath_input, file)
@@ -34,7 +56,7 @@ if __name__ == '__main__':
     t1 = time.time() * 1000
     #time.sleep(1) #1s
     fpath_input = "/home/ziyi/projects/ark+=b7291=d17p46_version_5/WeAllWalkv4/WeAllWalkToPublish8-20-17/SENSORDATA"
-    fpath_output = "~/projects/test/"
+    fpath_output = "~/projects/test"
     copy_and_rename(fpath_input, fpath_output)
     t2 = time.time() * 1000
     print('take time:' + str(t2 - t1) + 'ms')
